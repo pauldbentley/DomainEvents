cc26346cefb9a956bc58e77b3ad2c5eae06b6edc
pauldbentley <paul@paulbentley.co.uk>
2020-08-06 10:26:23 +0100

    Reorganise folders and remove classes from abstractions

--- src/Pdb.DomainEvents.Abstractions/DomainEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pdb.DomainEvents.Abstractions
-{
-    using System;
-
-    public abstract class DomainEvent : IDomainEvent
-    {
-        public DateTimeOffset DateOccurred { get; protected set; } = DateTime.UtcNow;
-    }
-}
